db8acc825fe9d234547deddadd35e97d92b48f76
cpiresci <cpiresci@gmail.com>
2026-06-05 03:05:22 -0300

    Add new routes for additional pages in App component

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,13 @@ import AuthModal from './components/AuthModal';
 import Toast from './components/Toast';
 import HomePage from './pages/Home';
 import HistoricoPage from './pages/Historico';
+import DeltaPage from './pages/Delta';
+import DocumentosPage from './pages/Documentos';
+import PeticoesPage from './pages/Peticoes';
+import SimuladorPage from './pages/Simulador';
+import MonitoramentoPage from './pages/Monitoramento';
+import VerificarPage from './pages/Verificar';
+import CheckoutPage from './pages/Checkout';
 import { useStore } from './store';
 
 export default function App() {
@@ -17,8 +24,15 @@ export default function App() {
         <Navbar />
         <main>
           <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/historico" element={<HistoricoPage />} />
+            <Route path="/"              element={<HomePage />} />
+            <Route path="/historico"     element={<HistoricoPage />} />
+            <Route path="/delta"         element={<DeltaPage />} />
+            <Route path="/documentos"    element={<DocumentosPage />} />
+            <Route path="/peticoes"      element={<PeticoesPage />} />
+            <Route path="/simulador"     element={<SimuladorPage />} />
+            <Route path="/monitoramento" element={<MonitoramentoPage />} />
+            <Route path="/verificar"     element={<VerificarPage />} />
+            <Route path="/premium"       element={<CheckoutPage />} />
           </Routes>
         </main>
       </div>
